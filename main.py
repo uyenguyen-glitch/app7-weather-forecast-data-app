fda7e3f44ce259a1adcf2510caa24bc332680343
uyenguyen-glitch <uyenguyen2000@gmail.com>
2023-05-10 21:52:48 +0700

    Day 33: Add sky condition, add try/except to not show KeyError and adjust temperature axis to show decimal values

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import plotly.express as px
+from backend import get_data
 
+# Add widgets
 st.title("Weather Forecast For The Next Days")
 place = st.text_input("Place")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
@@ -9,13 +11,25 @@ option = st.selectbox("Select data to view",
                       ("Temperature", "Sky"))
 st.subheader(f"{option} for the next {days} days in {place}")
 
-def get_data(days):
-    dates = ["2023-04-28", "2023-04-29", "2023-04-30"]
-    temperatures = [10, 11, 15]
-    temperatures = [days * i for i in temperatures]
-    return dates, temperatures
 
-d,t = get_data(days)
 
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature (C)"})
-st.plotly_chart(figure)
+
+if place:
+    try:
+        # Get the temperature/sky data
+        filtered_data = get_data(place, days)
+        if option == "Temperature":
+             temperature = [dict["main"]["temp"] / 10for dict in filtered_data]
+             dates = [dict["dt_txt"] for dict in filtered_data]
+
+             # Create a temperature plot
+             figure = px.line(x=dates, y=temperature, labels={"x": "Date", "y": "Temperature (C)"})
+             st.plotly_chart(figure)
+
+        if option == "Sky":
+            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
+            images = {"Clear": "images/clear.png", "Rain": "images/rain.png", "Snow": "images/snow.png", "Clouds": "images/cloud.png"}
+            img_paths = [images[condition] for condition in sky_conditions]
+            st.image(img_paths, width=111)
+    except KeyError:
+        st.write("Your city is not correct!!!")
